7a963dd8fc473ad69988bc6a372beceaf3e5a585
AniketRoy <aniket.roy@stu.utm.ac.in>
2017-10-20 11:28:02 +0530

    modifying probotPlugin function

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const handlePullRequestChange = require('./lib/handle-pull-request-change')
 
 function probotPlugin (robot) {
   robot.on([
-    'pull_request.opened',
-    'pull_request.edited',
-    'pull_request.synchronize'
+	'pull_request.opened',
+	'pull_request.edited',
+	'pull_request.synchronize'
   ], handlePullRequestChange.bind(null, robot))
 }
