82111e8246942f3278e291710407153db06c29b9
Gregor <gregor@martynus.net>
2018-07-05 11:53:32 -0700

    test: logging with context.{log,error}

--- test/unit/lib/handle-pull-request-change.test.js
+++ test/unit/lib/handle-pull-request-change.test.js
@@ -1,10 +1,13 @@
 const handlePullRequestChange = require('../../../lib/handle-pull-request-change')
 
 describe('handlePullRequestChange', () => {
-  console.log = jest.fn()
   const createMockContext = prTitle => {
     return {
       repo: jest.fn(),
+      log: {
+        info: jest.fn(),
+        error: jest.fn()
+      },
       payload: {
         pull_request: {
           head: { sha: 'sha' },
@@ -12,6 +15,11 @@ describe('handlePullRequestChange', () => {
           labels: [],
           number: 123,
           html_url: 'https://github.com/owner/repo/pull/123'
+        },
+        repository: {
+          owner: {
+            id: 1
+          }
         }
       },
       github: {
@@ -75,7 +83,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(pendingStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "[wip] Pull request title" — pending')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': true}, 'title': '[wip] Pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('creates pending status if PR title contains `WIP`', async () => {
@@ -83,7 +91,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(pendingStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "[WIP] Pull request title" — pending')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': true}, 'title': '[WIP] Pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('creates pending status if PR title contains `do not merge` case insensitive', async () => {
@@ -91,7 +99,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(pendingStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "foo dO NoT mERGe bar" — pending')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': true}, 'title': 'foo dO NoT mERGe bar', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('creates success status if PR title contains `work in progress`', async () => {
@@ -99,7 +107,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(pendingStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "Pull request title – work in progress" — pending')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': true}, 'title': 'Pull request title – work in progress', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('creates success status if PR title does NOT contain `wip`', async () => {
@@ -107,7 +115,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(successStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "[xxx] Pull request title" — success')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': false}, 'title': '[xxx] Pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('creates pending status if a commit message contains `wip`', async () => {
@@ -115,7 +123,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(pendingStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "Default pull request title" — pending')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': true}, 'title': 'Default pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('creates pending status if a commit message contains `do not merge`', async () => {
@@ -123,7 +131,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(pendingStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "Default pull request title" — pending')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': true}, 'title': 'Default pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('creates success status if a commit message does not contain `wip` or `do not merge`', async () => {
@@ -131,7 +139,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(successStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "Default pull request title" — success')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': false}, 'title': 'Default pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('creates pending status if a label contains `wip`', async () => {
@@ -139,7 +147,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(pendingStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "Default pull request title" — pending')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': true}, 'title': 'Default pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('creates pending status if a label contains `do not merge`', async () => {
@@ -147,7 +155,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(pendingStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "Default pull request title" — pending')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': true}, 'title': 'Default pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('creates success status if a label does not contain `wip` or `do not merge`', async () => {
@@ -155,7 +163,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.repo).lastCalledWith(successStatusObject)
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "Default pull request title" — success')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': true, 'wip': false}, 'title': 'Default pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('does not create a status if there is no change', async () => {
@@ -163,7 +171,7 @@ describe('handlePullRequestChange', () => {
     await handlePullRequestChange(context)
 
     expect(context.github.repos.createStatus).not.toHaveBeenCalled()
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "[wip] Pull request title" — pending (unchanged)')
+    expect(context.log.info).lastCalledWith({'accountId': 1, 'status': {'changed': false, 'wip': true}, 'title': '[wip] Pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 
   it('handles error', async () => {
@@ -171,6 +179,6 @@ describe('handlePullRequestChange', () => {
     context.github.repos.createStatus = jest.fn().mockRejectedValue(new Error('boom banana'))
     await handlePullRequestChange(context)
 
-    expect(console.log).lastCalledWith('https://github.com/owner/repo/pull/123 "[wip] Pull request title" — Error: boom banana')
+    expect(context.log.error).lastCalledWith({'accountId': 1, 'error': {'code': undefined, 'message': 'boom banana', 'name': 'Error'}, 'status': {'changed': true, 'wip': true}, 'title': '[wip] Pull request title', 'url': 'https://github.com/owner/repo/pull/123'})
   })
 })
