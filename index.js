fd1f3bd4286e362409951b8b4906f597f9a23509
Gregor <gregor@martynus.net>
2018-10-12 21:02:58 -0700

    feat: marketplace version
    BREAKING CHANGE: WIP now differentiates between a free and a pro plan. The latter now supports configuration of terms and locations to look at. The new version also uses checks instead of commit statuses

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-module.exports = probotPlugin
+module.exports = wip
 
-const sendLogs = require('./lib/send-logs')
+const sendLogs = require('./lib/logs/send')
 const handlePullRequestChange = require('./lib/handle-pull-request-change')
+const handleRequestedAction = require('./lib/handle-requested-action')
 
-function probotPlugin (robot) {
-  robot.on([
+function wip (app) {
+  // listen to all relevant pull request event actions
+  app.on([
     'pull_request.opened',
     'pull_request.edited',
     'pull_request.labeled',
     'pull_request.unlabeled',
     'pull_request.synchronize'
-  ], handlePullRequestChange)
+  ], handlePullRequestChange.bind(null, app))
 
-  sendLogs(robot)
+  // listen to an overwrite request action from a check run
+  app.on('check_run.requested_action', handleRequestedAction.bind(null, app))
+
+  sendLogs(app)
 }
---
+++ pro-plan-for-free.js
@@ -0,0 +1,9 @@
+// WIP is a not-for-profit application. All revenue from the "pro" plan is
+// donated, see "About WIP" in the README.md file.
+//
+// If you cannot pay for the "pro" plan but need the additional features, add
+// your user or organization account with a comment explanation.
+
+module.exports = [
+  'gr2m' // 2018-10-11: It me! I made WIP and added my account here for testing
+]
