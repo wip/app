6bc0a741094bc5b60da7989397e137b44df2bad7
Steve Winton <stevewinton@gmail.com>
2019-05-07 20:07:02 -0500

    Add getPlan tests for GitHub Enterprise Server

--- test/unit/get-plan-test.js
+++ test/unit/get-plan-test.js
@@ -1,5 +1,18 @@
 const proxyquire = require('proxyquire').noCallThru()
-const { test } = require('tap')
+const { beforeEach, afterEach, test } = require('tap')
+
+beforeEach((done, t) => {
+  // Preserve GHE_HOST value before removal
+  t.context.GHE_HOST = process.env.GHE_HOST
+  delete process.env.GHE_HOST
+  done()
+})
+
+afterEach((done, t) => {
+  // Restore initial GHE_HOST value
+  process.env.GHE_HOST = t.context.GHE_HOST
+  done()
+})
 
 test('returns "pro" if account is enabled manually', async function (t) {
   const getPlan = proxyquire('../../lib/get-plan', {
@@ -7,6 +20,18 @@ test('returns "pro" if account is enabled manually', async function (t) {
   })
   const app = {}
   const owner = { login: 'foo' }
+  const plan = await getPlan(app, owner)
+
+  t.is(plan, 'pro')
+  t.end()
+})
+
+test('returns "pro" for GitHub Enterprise Server installations', async function (t) {
+  const getPlan = require('../../lib/get-plan')
+  const app = {}
+  const owner = { login: 'foo' }
+
+  process.env.GHE_HOST = true
   const plan = await getPlan(app, owner)
 
   t.is(plan, 'pro')
