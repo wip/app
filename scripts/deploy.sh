b0de28be6f17280d8c97f3edbb692f92c434820a
Gregor <gregor@martynus.net>
2019-05-31 11:52:46 -0700

    ci: deploy to beta

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,8 @@ set -e
 
 now="npx now --debug --token=$NOW_TOKEN"
 
-echo "$ now rm --safe --yes wip"
-$now rm --safe --yes wip
+echo "$ now rm --safe --yes wip-beta"
+$now rm --safe --yes wip-beta
 
 # https://github.com/zeit/now-cli/blob/master/errors/verification-timeout.md
 echo "$ now --no-verify"
