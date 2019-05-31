669407dcef96ae12227e4e5d0308d335eb6ca052
Gregor <gregor@martynus.net>
2019-05-31 13:33:31 -0700

    test: adapt for deprecation of legacy commit status support

--- test/integration/legacy-test.js
+++ test/integration/legacy-test.js
@@ -38,163 +38,27 @@ beforeEach(function (done) {
 test('new pull request with "Test" title', async function (t) {
   await this.app.receive(require('./events/new-pull-request-with-test-title.json'))
 
-  // check getting current status
-  t.is(this.githubMock.repos.getCombinedStatusForRef.callCount, 1)
-  const getStatusParams = this.githubMock.repos.getCombinedStatusForRef.lastCall.arg
-  t.is(getStatusParams.owner, 'wip')
-  t.is(getStatusParams.repo, 'app')
-  t.is(getStatusParams.ref, 'sha123')
+  // check setting new status
+  t.is(this.githubMock.repos.createStatus.callCount, 1)
+  t.is(this.githubMock.repos.createStatus.lastCall.arg.target_url, 'https://github.com/organizations/wip/settings/installations/1/permissions/update')
+
+  // check resulting logs
+  t.is(this.logMock.info.lastCall.arg, '⛔ wip/app#1 (legacy)')
+  t.is(this.logMock.child.lastCall.arg.legacy, true)
+
+  t.end()
+})
+
+test('new pull request with "Test" title from user', async function (t) {
+  await this.app.receive(require('./events/new-pull-request-with-test-title-from-user.json'))
 
   // check setting new status
   t.is(this.githubMock.repos.createStatus.callCount, 1)
+  t.is(this.githubMock.repos.createStatus.lastCall.arg.target_url, 'https://github.com/settings/installations/1/permissions/update')
 
   // check resulting logs
-  t.is(this.logMock.info.lastCall.arg, '✅ wip/app#1 (legacy)')
-  const logParams = this.logMock.child.lastCall.arg
-  t.is(logParams.wip, false)
-  t.is(logParams.change, true)
-  t.is(logParams.legacy, true)
-
-  t.end()
-})
-
-test('new pull request with "[WIP] Test" title', async function (t) {
-  await this.app.receive(require('./events/new-pull-request-with-wip-title.json'))
-
-  // check resulting logs
-  t.is(this.logMock.info.lastCall.arg, '⏳ wip/app#1 (legacy)')
-  const logParams = this.logMock.child.lastCall.arg
-  t.is(logParams.wip, true)
-  t.is(logParams.change, true)
-  t.is(logParams.legacy, true)
-
-  t.end()
-})
-
-test('new pull request with "foo dO NoT mERGe bar" title', async function (t) {
-  await this.app.receive(require('./events/new-pull-request-with-do-not-merge-title.json'))
-
-  // check resulting logs
-  t.is(this.logMock.info.lastCall.arg, '⏳ wip/app#1 (legacy)')
-  const logParams = this.logMock.child.lastCall.arg
-  t.is(logParams.wip, true)
-  t.is(logParams.change, true)
-  t.is(logParams.legacy, true)
-
-  t.end()
-})
-
-test('new pull request with "[Work in Progress] Test" title', async function (t) {
-  await this.app.receive(require('./events/new-pull-request-with-work-in-progress-title.json'))
-
-  // check resulting logs
-  t.is(this.logMock.info.lastCall.arg, '⏳ wip/app#1 (legacy)')
-  const logParams = this.logMock.child.lastCall.arg
-  t.is(logParams.wip, true)
-  t.is(logParams.change, true)
-  t.is(logParams.legacy, true)
-
-  t.end()
-})
-
-test('new pull request with "WIP" in commit', async function (t) {
-  // commit with "WIP: test" subject
-  this.githubMock.pullRequests.listCommits = simple.mock().resolveWith({
-    data: [{
-      commit: {
-        message: 'WIP: test'
-      }
-    }]
-  })
-
-  await this.app.receive(require('./events/new-pull-request-with-test-title.json'))
-
-  // check resulting logs
-  t.is(this.logMock.info.lastCall.arg, '⏳ wip/app#1 (legacy)')
-  const logParams = this.logMock.child.lastCall.arg
-  t.is(logParams.wip, true)
-  t.is(logParams.change, true)
-  t.is(logParams.legacy, true)
-
-  t.end()
-})
-
-test('new pull request with "DO NOT MERGE" in commit', async function (t) {
-  // commit with "DO NOT MERGE: test" subject
-  this.githubMock.pullRequests.listCommits = simple.mock().resolveWith({
-    data: [{
-      commit: {
-        message: 'DO NOT MERGE: test'
-      }
-    }]
-  })
-
-  await this.app.receive(require('./events/new-pull-request-with-test-title.json'))
-
-  // check resulting logs
-  t.is(this.logMock.info.lastCall.arg, '⏳ wip/app#1 (legacy)')
-  const logParams = this.logMock.child.lastCall.arg
-  t.is(logParams.wip, true)
-  t.is(logParams.change, true)
-  t.is(logParams.legacy, true)
-
-  t.end()
-})
-
-test('new pull request without "WIP" in commit', async function (t) {
-  // commit with "DO NOT MERGE: test" subject
-  this.githubMock.pullRequests.listCommits = simple.mock().resolveWith({
-    data: [{
-      commit: {
-        message: 'foo'
-      }
-    }]
-  })
-
-  await this.app.receive(require('./events/new-pull-request-with-test-title.json'))
-
-  // check resulting logs
-  t.is(this.logMock.info.lastCall.arg, '✅ wip/app#1 (legacy)')
-  const logParams = this.logMock.child.lastCall.arg
-  t.is(logParams.wip, false)
-  t.is(logParams.change, true)
-  t.is(logParams.legacy, true)
-
-  t.end()
-})
-
-test('new pull request with "WIP" in label', async function (t) {
-  await this.app.receive(require('./events/new-pull-request-with-wip-label.json'))
-
-  // check resulting logs
-  t.is(this.logMock.info.lastCall.arg, '⏳ wip/app#1 (legacy)')
-  const logParams = this.logMock.child.lastCall.arg
-  t.is(logParams.wip, true)
-  t.is(logParams.change, true)
-  t.is(logParams.legacy, true)
-
-  t.end()
-})
-
-test('does not create a status if there is no change', async function (t) {
-  // return "pending" status
-  this.githubMock.repos.getCombinedStatusForRef = simple.mock().resolveWith({
-    data: {
-      statuses: [{
-        context: 'WIP',
-        state: 'pending'
-      }]
-    }
-  })
-
-  await this.app.receive(require('./events/new-pull-request-with-wip-title.json'))
-
-  // check resulting logs
-  t.is(this.logMock.info.lastCall.arg, '😐 wip/app#1 (legacy)')
-  const logParams = this.logMock.child.lastCall.arg
-  t.is(logParams.wip, true)
-  t.is(logParams.change, false)
-  t.is(logParams.legacy, true)
+  t.is(this.logMock.info.lastCall.arg, '⛔ wip/app#1 (legacy)')
+  t.is(this.logMock.child.lastCall.arg.legacy, true)
 
   t.end()
 })
@@ -208,19 +72,6 @@ test('request error', async function (t) {
 
   // check resulting logs
   t.same(this.logMock.error.lastCall.arg, SERVER_ERROR)
-
-  t.end()
-})
-
-test('request error (without code being parsed, see octokit/rest.js#684)', async function (t) {
-  // simulate request error
-  this.githubMock.repos.createStatus = simple.mock().rejectWith(new Error('{"status": 123}'))
-  this.logMock.error = simple.mock()
-
-  await this.app.receive(require('./events/new-pull-request-with-test-title.json'))
-
-  // check resulting logs
-  t.same(this.logMock.error.lastCall.arg.status, 123)
 
   t.end()
 })
