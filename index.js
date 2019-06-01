e85727af56fc36b2781820584df14541e6d0931e
Gregor <gregor@martynus.net>
2019-05-31 21:59:10 -0700

    feat: handle installation events – update all open Pull Requests

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const sendLogs = require('./lib/logs/send')
 const logMemoryUsage = require('./lib/logs/memory-usage.js')
 const handlePullRequestChange = require('./lib/handle-pull-request-change')
 const handleMarketplacePurchase = require('./lib/handle-marketplace-purchase')
+const handleInstallation = require('./lib/handle-installation')
 
 function wip (app) {
   // listen to all relevant pull request event actions
@@ -17,6 +18,10 @@ function wip (app) {
 
   // listen to marketplace events
   app.on('marketplace_purchase', handleMarketplacePurchase.bind(null, app))
+
+  // listen
+  app.on('installation', handleInstallation.bind(null, app))
+  app.on('installation_repositories', handleInstallation.bind(null, app))
 
   sendLogs(app)
   logMemoryUsage(app)
