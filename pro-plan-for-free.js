3be448d738030a78b82750f95228b2b2693c988d
Nguyễn Minh Tuấn <39961250+MoonSmile1707@users.noreply.github.com>
2019-09-02 00:25:16 +0700

    Add @MoonSmile1707 to pro-plan-for-free.js (#209)

--- pro-plan-for-free.js
+++ pro-plan-for-free.js
@@ -7,5 +7,6 @@
 module.exports = [
   'resistbot', // 2018-10-23: Volunteer run org, helping people engage in their democracy
   'itspugle', // 2018-10-24: Student creating tools to better enable volunteer groups,
-  'urbanengine' // 2019-03-29: non-profit organization helping startups & small businesses network and grow within their community
+  'urbanengine', // 2019-03-29: non-profit organization helping startups & small businesses network and grow within their community
+  'moonsmile' // 2019-9-1: Student writing tools to learn, I promise i will pay when I finish my project.
 ]
