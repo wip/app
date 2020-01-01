bdd4400f6106a6c4903797bdd663af9e0c098e67
Kaxil Naik <kaxilnaik@gmail.com>
2020-01-01 19:11:50 +0000

    Add Apache Organization to pro-plan-for-free.js (#220)

--- pro-plan-for-free.js
+++ pro-plan-for-free.js
@@ -8,5 +8,6 @@ module.exports = [
   'resistbot', // 2018-10-23: Volunteer run org, helping people engage in their democracy
   'itspugle', // 2018-10-24: Student creating tools to better enable volunteer groups,
   'urbanengine', // 2019-03-29: non-profit organization helping startups & small businesses network and grow within their community
-  'moonsmile' // 2019-9-1: Student writing tools to learn, I promise i will pay when I finish my project.
+  'moonsmile', // 2019-9-1: Student writing tools to learn, I promise i will pay when I finish my project.
+  'apache' // 2020-01-01: non-profit corporation to support Apache software projects
 ]
